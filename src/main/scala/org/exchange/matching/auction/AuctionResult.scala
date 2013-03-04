6cda1411fe3e332fda1be96caf7d2d66d739b045
Frank Ratzlow <ratzlow@gmail.com>
2013-03-04 08:05:10 +0100

    added some scala doc

--- src/main/scala/org/exchange/matching/auction/AuctionResult.scala
+++ src/main/scala/org/exchange/matching/auction/AuctionResult.scala
@@ -3,8 +3,12 @@ package org.exchange.matching.auction
 import org.exchange.model.Orderbook
 
 /**
- * TODO: comment
+ * Aggregates all parameters that resulted from an auction of a given orderbook.
  *
+ *
+ * @param orderbook after the auction was performed. Contains all the orders that could not be executed or were not
+ *                  considered for the price finding process.
+ * @param referencePrice criteria to find auction price to use for the auction // TODO (FRa) : (FRa) : more details
  * @param lowestVisibleAskLimit limit on ask side nearest to the market if no auction price can be determined
  * @param highestVisibleBidLimit limit on bid side nearest to the market if no auction price can be determined
  *
